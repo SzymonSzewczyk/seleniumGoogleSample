f876b6373a1fdc9013226582eb047ce9939719d7
Szymon Szewczyk <szewczykszymon@wp.pl>
2021-11-30 14:34:28 +0100

    added junit dependency; up BrowserActions added openURL(), findElement(), waitABit()

--- src/main/java/tools/BrowserActions.java
+++ src/main/java/tools/BrowserActions.java
@@ -1,11 +1,12 @@
 package tools;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
 
 public class BrowserActions {
 	private static final Logger logger = LoggerFactory.getLogger(BrowserActions.class);
@@ -34,7 +35,7 @@ public class BrowserActions {
 			 currentTime = System.currentTimeMillis()) {
 			try {
 				Thread.sleep(500L);
-				this.logger.info("Sleeping while page status is: " + ((JavascriptExecutor) this.driver).executeScript("return document.readyState", new Object[0]));
+				logger.info("Sleeping while page status is: " + ((JavascriptExecutor) getDriver()).executeScript("return document.readyState", new Object[0]));
 			} catch (InterruptedException e) {
 				logger.error("Interrupted while waiting");
 			}
@@ -51,4 +52,52 @@ public class BrowserActions {
 		}
 		return result;
 	}
+
+	public void openURL(String url) {
+		try {
+			logger.info("Opening page URL: " + url);
+			getDriver().navigate().to(url);
+			getDriver().manage().window().maximize();
+			logger.info("Page was opened and window maximized");
+		} catch (WebDriverException webDriverException) {
+			logger.error(String.format("Could not navigate to URL: %s ", url));
+		}
+	}
+
+	public WebElement findElement(By by) {
+		boolean isFound = false;
+		WebElement element = null;
+		LocalTime start = LocalTime.now();
+
+		while (true) {
+			try {
+				List<WebElement> elements = getDriver().findElements(by);
+				if (elements.size() <= 0) {
+					throw new NoSuchElementException(String.format("Element matching criteria: %s was not found!", by));
+				}
+
+				element = elements.get(0);
+				isFound = true;
+			} catch (StaleElementReferenceException e) {
+				if (Duration.between(start, LocalTime.now()).getSeconds() >= 5L) {
+					throw new Error(String.format("Stale element ref found! after %s sec", 5));
+				}
+
+				waitABit(500);
+				if (isFound) {
+					break;
+				}
+			}
+		}
+		return element;
+	}
+
+	private void waitABit(int timeOutInMilliSeconds) {
+		try {
+			Thread.sleep(timeOutInMilliSeconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
