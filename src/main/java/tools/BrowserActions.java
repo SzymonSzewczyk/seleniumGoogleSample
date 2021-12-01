7112ff5aa8c2058c6dc83022c2b971eb5f58693b
Szymon Szewczyk <szewczykszymon@wp.pl>
2021-12-01 16:39:32 +0100

    up TimeOuts.java with few fields; up BrowserActions.java with waitForElementToBeVisible, findAllElements, sendKeys, setEditJavaScript, click, clickJavaScript

--- src/main/java/tools/BrowserActions.java
+++ src/main/java/tools/BrowserActions.java
@@ -1,11 +1,14 @@
 package tools;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BrowserActions {
@@ -64,6 +67,16 @@ public class BrowserActions {
 		}
 	}
 
+	public void waitForElementToBeVisible(By fieldLocator) {
+		waitForElementToBeVisible(fieldLocator, Duration.ofSeconds(10));
+	}
+
+	public void waitForElementToBeVisible(By fieldLocator, Duration maxSecondsTimeout) {
+		WebDriverWait wait = new WebDriverWait(getDriver(), maxSecondsTimeout);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(fieldLocator));
+
+	}
+
 	public WebElement findElement(By by) {
 		boolean isFound = false;
 		WebElement element = null;
@@ -92,12 +105,123 @@ public class BrowserActions {
 		return element;
 	}
 
-	private void waitABit(int timeOutInMilliSeconds) {
+	public List<WebElement> findAllElements(By by) {
+		List<WebElement> elements = new ArrayList<>();
+		LocalTime start = LocalTime.now();
+
+		while (true) {
+			try {
+				elements = getDriver().findElements(by);
+				break;
+			} catch (StaleElementReferenceException exc) {
+				boolean staleExc = true;
+				if (Duration.between(start, LocalTime.now()).getSeconds() >= 5L) {
+					throw new Error(String.format("Stale element ref found! after %s sec", 5));
+				}
+
+				waitABit(500);
+				if (!staleExc) {
+					break;
+				}
+			}
+		}
+		return elements;
+	}
+
+	public void sendKeys(By fieldLocator, String value) {
+		LocalTime start = LocalTime.now();
+
+		try {
+			waitForElementToBeVisible(fieldLocator);
+			if (isElementVisible(fieldLocator)) {
+				findElement(fieldLocator).clear();
+				findElement(fieldLocator).sendKeys(new CharSequence[]{value});
+			} else {
+				setEditJavaScript(fieldLocator, value);
+			}
+
+			logger.info(String.format("EditBox : %s was filled with value %s", fieldLocator, value));
+		} catch (StaleElementReferenceException exc) {
+			if (Duration.between(start, LocalTime.now()).getSeconds() >= 5L) {
+				throw new Error(String.format("Stale element ref found while setting EditBox: %s with value: %s", fieldLocator, value));
+			}
+			waitABit(1000);
+			sendKeys(fieldLocator, value);
+		}
+	}
+
+	public void setEditJavaScript(By fieldLocator, String value) {
+		LocalTime start = LocalTime.now();
+
+		try {
+			JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+			executor.executeScript("arguments[0].setAttribute('value', '" + value + "')", findElement(fieldLocator));
+		} catch (StaleElementReferenceException exc) {
+			if (Duration.between(start, LocalTime.now()).getSeconds() >= TimeOuts.TIMEOUT_5) {
+				throw new Error(String.format("Stale element ref found while setting (JavaScript) EditBox: %s with value: %s", fieldLocator, value));
+			} else {
+				waitABit(500);
+				setEditJavaScript(fieldLocator, value);
+			}
+		}
+	}
+
+	public void click(By byLocator) {
+		LocalTime start = LocalTime.now();
+		waitForElementToBeVisible(byLocator);
+		try {
+			if (isElementVisible(byLocator)) {
+				findElement(byLocator).click();
+				logger.info("Element : '" + byLocator + "' was clicked..");
+			} else {
+				waitForPageToLoad(TimeOuts.TIMEOUT_10);
+				clickJavaScript(byLocator);
+				logger.info("Element: '" + byLocator + "' was clicked by JavaScript..");
+			}
+		} catch (StaleElementReferenceException e) {
+			if (Duration.between(start, LocalTime.now()).getSeconds() >= TimeOuts.TIMEOUT_5) {
+				throw new Error(String.format("Stale element ref found while clicking on: %s ", byLocator));
+			} else {
+				waitABit(TimeOuts.TIMEOUT_500);
+				click(byLocator);
+			}
+		} catch (WebDriverException e) {
+			if (e.getMessage().contains("is not clickable at point")) {
+				clickJavaScript(byLocator);
+			} else {
+				throw new WebDriverException(String.format("Could not click on element %s , Error: ", byLocator, e.getMessage()));
+			}
+		}
+	}
+
+	public void clickJavaScript(By byLocator) {
+		LocalTime start = LocalTime.now();
+		try {
+			WebElement element = findElement(byLocator);
+			JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+			executor.executeScript("argument[0].click();", element);
+		} catch (StaleElementReferenceException e) {
+			if (Duration.between(start, LocalTime.now()).getSeconds() >= TimeOuts.TIMEOUT_5) {
+				throw new Error(String.format("Stale element ref found while clicking (JavaScript) on: %s ", byLocator));
+			} else {
+				waitABit(TimeOuts.TIMEOUT_500);
+				clickJavaScript(byLocator);
+			}
+		}
+	}
+
+	public void clear(By byIdentifier) {
+		TODO
+	}
+
+
+	public void waitABit(int timeOutInMilliSeconds) {
 		try {
 			Thread.sleep(timeOutInMilliSeconds);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
+
 
 }
--- src/main/java/tools/TimeOuts.java
+++ src/main/java/tools/TimeOuts.java
@@ -2,15 +2,30 @@ package tools;
 
 public class TimeOuts {
 
-	public final static int TIMEOUT_1 = 1;
-	public final static int TIMEOUT_5 = 5;
-	public final static int TIMEOUT_10 = 10;
-	public final static int TIMEOUT_30 = 30;
-	public final static int TIMEOUT_60 = 60;
+	public static final int TIMEOUT_1 = 1;
+	public static final int TIMEOUT_5 = 5;
+	public static final int TIMEOUT_10 = 10;
+	public static final int TIMEOUT_30 = 30;
+	public static final int TIMEOUT_60 = 60;
 
-	public final static int TIMEOUT_100 = 100;
-	public final static int TIMEOUT_200 = 200;
-	public final static int TIMEOUT_500 = 500;
-	public final static int TIMEOUT_1000 = 1000;
+	public static final int TIMEOUT_100 = 100;
+	public static final int TIMEOUT_200 = 200;
+	public static final int TIMEOUT_500 = 500;
+	public static final int TIMEOUT_1000 = 1000;
+
+	//in milliseconds
+	public static final int MS_1MIN = 60000;
+	public static final int MS_2MIN = 120000;
+	public static final int MS_5MIN = 300000;
+	public static final int MS_10MIN = 600000;
+	public static final int MS_DEFAULT_FOR_ELEMENT_APPEARS_VISIBLE = 100000; //10s
+
+	//in seconds
+	public static final int S_DEFAULT_FOR_ELEMENT_APPEARS_VISIBLE = MS_DEFAULT_FOR_ELEMENT_APPEARS_VISIBLE * 60;
+
+
+	public long getInMilliseconds(int seconds) {
+		return seconds * 1000L;
+	}
 
 }
