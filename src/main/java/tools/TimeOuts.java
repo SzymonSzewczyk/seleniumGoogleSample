e43a5b5729f87294e59fff57f210a995d7c2369f
Szymon Szewczyk <szewczykszymon@wp.pl>
2021-12-01 14:38:22 +0100

    added TimeOuts

---
+++ src/main/java/tools/TimeOuts.java
@@ -0,0 +1,16 @@
+package tools;
+
+public class TimeOuts {
+
+	public final static int TIMEOUT_1 = 1;
+	public final static int TIMEOUT_5 = 5;
+	public final static int TIMEOUT_10 = 10;
+	public final static int TIMEOUT_30 = 30;
+	public final static int TIMEOUT_60 = 60;
+
+	public final static int TIMEOUT_100 = 100;
+	public final static int TIMEOUT_200 = 200;
+	public final static int TIMEOUT_500 = 500;
+	public final static int TIMEOUT_1000 = 1000;
+
+}
